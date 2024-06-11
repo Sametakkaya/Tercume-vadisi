2b89ad92b4d34b63bb4b01038c82b86df3e8faf5
Samet Akkaya <102583793+Sametakkaya@users.noreply.github.com>
2024-06-11 23:52:40 +0300

    İlk commit: Proje başlatıldı

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
    // XMLHttpRequest kullanarak üst navbar dosyasını yükleme
     var xhrTopNavbar = new XMLHttpRequest();
-    xhrTopNavbar.open('GET', '../navbar/top-navbar.html', true);
+    xhrTopNavbar.open('GET', './navbar/top-navbar.html', true);
     xhrTopNavbar.onreadystatechange = function () {
       if (xhrTopNavbar.readyState === 4 && xhrTopNavbar.status === 200) {
         document.getElementById('top-navbar-container').innerHTML = xhrTopNavbar.responseText;
@@ -10,7 +10,7 @@
 
  // XMLHttpRequest kullanarak alt navbar dosyasını yükleme
     var xhrNavbar = new XMLHttpRequest();
-    xhrNavbar.open('GET', '/navbar/navbar.html', true);
+    xhrNavbar.open('GET', './navbar/navbar.html', true);
     xhrNavbar.onreadystatechange = function () {
       if (xhrNavbar.readyState === 4 && xhrNavbar.status === 200) {
         document.getElementById('navbar-container').innerHTML = xhrNavbar.responseText;
