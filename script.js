9a4803fa6cde756b0b4f669171d5ae38aed8b60d
Samet Akkaya <102583793+Sametakkaya@users.noreply.github.com>
2024-06-11 18:34:16 +0300

    Hizmetler Kısmı Ekleme

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+   // XMLHttpRequest kullanarak üst navbar dosyasını yükleme
+    var xhrTopNavbar = new XMLHttpRequest();
+    xhrTopNavbar.open('GET', 'top-navbar.html', true);
+    xhrTopNavbar.onreadystatechange = function () {
+      if (xhrTopNavbar.readyState === 4 && xhrTopNavbar.status === 200) {
+        document.getElementById('top-navbar-container').innerHTML = xhrTopNavbar.responseText;
+      }
+    };
+    xhrTopNavbar.send();
+
+ // XMLHttpRequest kullanarak alt navbar dosyasını yükleme
+    var xhrNavbar = new XMLHttpRequest();
+    xhrNavbar.open('GET', 'navbar.html', true);
+    xhrNavbar.onreadystatechange = function () {
+      if (xhrNavbar.readyState === 4 && xhrNavbar.status === 200) {
+        document.getElementById('navbar-container').innerHTML = xhrNavbar.responseText;
+      }
+    };
+    xhrNavbar.send();
+
+
+    // Footer Script //
+  // XMLHttpRequest kullanarak üst navbar dosyasını yükleme
+  var xhrFooter = new XMLHttpRequest();
+  xhrFooter.open('GET', 'footer.html', true);
+  xhrFooter.onreadystatechange = function () {
+   if (xhrFooter.readyState === 4 && xhrFooter.status === 200) {
+    document.getElementById('footer-container').innerHTML = xhrFooter.responseText;
+   }
+  };
+  xhrFooter.send();
+
 function writeText(text) {
   document.getElementById("text").innerHTML = "";
   document.getElementById("text").innerHTML += text + "<br>";
